08ecc7d5db0e0714d79523f28d4a83a16d0ca67c
ArunZemoso <arunkumar.marella@zemosolabs.com>
2022-02-20 12:20:54 +0530

    2

--- homepage.js
+++ homepage.js
@@ -1,3 +1,20 @@
 let tables=document.getElementById("tables");
 let menu=document.getElementById("menu");
 
+let tableCount=3;
+
+function addTable(){
+    let table=document.createElement('div');
+    table.setAttribute("class","table");
+    let h1=document.createElement('h1');
+    h1.innerHTML=`Table-${++tableCount}`;
+    table.appendChild(h1);
+    let priceItemcount=document.createElement("div");
+    priceItemcount.setAttribute("class","price-itemCount")
+    table.appendChild(priceItemcount);
+    let rsText=document.createElement("div");
+    rsText.innerHTML="Rs."
+    priceItemcount.appendChild(rsText);
+    let totalPrice=createElement('h3')
+    totalPrice
+}

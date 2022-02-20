7edeb1443439d7c890e7660e08ec2e6198450d7a
ArunZemoso <arunkumar.marella@zemosolabs.com>
2022-02-20 13:14:45 +0530

    addTable function

--- homepage.js
+++ homepage.js
@@ -4,7 +4,9 @@ let menu=document.getElementById("menu");
 let tableCount=3;
 
 function addTable(){
+    console.log(tables);
     let table=document.createElement('div');
+    tables.appendChild(table);
     table.setAttribute("class","table");
     let h1=document.createElement('h1');
     h1.innerHTML=`Table-${++tableCount}`;
@@ -15,6 +17,18 @@ function addTable(){
     let rsText=document.createElement("div");
     rsText.innerHTML="Rs."
     priceItemcount.appendChild(rsText);
-    let totalPrice=createElement('h3')
-    totalPrice
+    let totalPrice=document.createElement('h3')
+    priceItemcount.appendChild(totalPrice);
+    totalPrice.setAttribute("id",`table${tableCount}-price`);
+    totalPrice.style.marginRight="20%"
+    totalPrice.innerHTML="0"
+    let totalItems=document.createElement("h3")
+    priceItemcount.appendChild(totalItems);
+    totalItems.innerHTML="Total items:"
+    let itemCount=document.createElement("h3")
+    priceItemcount.appendChild(itemCount)
+    priceItemcount.setAttribute("id",`table${tableCount}-itemCount`)
+    itemCount.innerHTML="0";
+    
 }
+addTable();

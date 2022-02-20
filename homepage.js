5548a348036bd7193ea60b844402d717c200c3cc
ArunZemoso <arunkumar.marella@zemosolabs.com>
2022-02-20 17:06:32 +0530

    loaded using text files

--- homepage.js
+++ homepage.js
@@ -1,26 +1,30 @@
 let tables=document.getElementById("tables");
 let menu=document.getElementById("menu");
 
-let tableCount=3;
+let tableCount=0;
+let tableData=[]
+let foodItems=[]
 
-function addTable(){
+
+function addTable(name,price){
+    tableCount++;
     let table=document.createElement('div');
     tables.appendChild(table);
     table.setAttribute("class","table");
-    let h1=document.createElement('h1');
-    h1.innerHTML=`Table-${++tableCount}`;
-    table.appendChild(h1);
+    let tableName=document.createElement('h1');
+    tableName.innerHTML=name;
+    table.appendChild(tableName);
     let priceItemcount=document.createElement("div");
     priceItemcount.setAttribute("class","price-itemCount")
     table.appendChild(priceItemcount);
-    let rsText=document.createElement("div");
+    let rsText=document.createElement("h3");
     rsText.innerHTML="Rs."
     priceItemcount.appendChild(rsText);
     let totalPrice=document.createElement('h3')
     priceItemcount.appendChild(totalPrice);
     totalPrice.setAttribute("id",`table${tableCount}-price`);
     totalPrice.style.marginRight="20%"
-    totalPrice.innerHTML="0"
+    totalPrice.innerHTML=price
     let totalItems=document.createElement("h3")
     priceItemcount.appendChild(totalItems);
     totalItems.innerHTML="Total items:"
@@ -31,17 +35,31 @@ function addTable(){
     
 }
 
-function addFoodItemToMenu(){
+function addFoodItemToMenu(name,cost){
     let foodItem=document.createElement("div")
     menu.appendChild(foodItem)
-    foodItem.setAttribute("class","foodItem ChickenBurger")
+    foodItem.setAttribute("class","foodItem "+name)
     let foodItemName=document.createElement("h1")
     foodItem.appendChild(foodItemName)
-    foodItemName.innerHTML="ChickenBurger"
+    foodItemName.innerHTML=name
     let foodItemPrice=document.createElement("h3");
     foodItem.appendChild(foodItemPrice)
-    foodItemPrice.innerHTML="99.00"
+    foodItemPrice.innerHTML=cost
 }
 
-addTable();
-addFoodItemToMenu();
+function loadData(){
+    fetch("./tables.txt").then(res=>res.text()).then(function(data){
+        tableData=JSON.parse(data)
+        console.log(tableData);
+        tableData.forEach(item=>{
+            addTable(item.tableName,item.totalPrice)
+        })
+    })
+    fetch("./foodItems.txt").then(res=>res.text()).then(function(data){
+        foodItems=JSON.parse(data)
+        foodItems.forEach(item=>{
+            addFoodItemToMenu(item.name,item.cost);
+        })
+    })
+}
+loadData();
